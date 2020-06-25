875a1c8b2bef80d8512a017fa43157ef884a9e64
ISvirko <ISvirko@yahoo.com>
2020-06-25 22:00:18 +0300

    Updates

---
+++ precache-manifest.5937229cf747b168f7d08066c1a1aadb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b03cab1579684fdcaac20e29a27d765",
+    "url": "/goit-react-hw-03-image-finder-infinite-scroll/index.html"
+  },
+  {
+    "revision": "6b727fec0b3d6df89eae",
+    "url": "/goit-react-hw-03-image-finder-infinite-scroll/static/css/2.1b60f930.chunk.css"
+  },
+  {
+    "revision": "60cd308f8451ccdd5b20",
+    "url": "/goit-react-hw-03-image-finder-infinite-scroll/static/css/main.c02ad852.chunk.css"
+  },
+  {
+    "revision": "6b727fec0b3d6df89eae",
+    "url": "/goit-react-hw-03-image-finder-infinite-scroll/static/js/2.06d1d934.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder-infinite-scroll/static/js/2.06d1d934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60cd308f8451ccdd5b20",
+    "url": "/goit-react-hw-03-image-finder-infinite-scroll/static/js/main.4e99bd72.chunk.js"
+  },
+  {
+    "revision": "7a3636549f1b0d50dad0",
+    "url": "/goit-react-hw-03-image-finder-infinite-scroll/static/js/runtime-main.1ad09aa1.js"
+  },
+  {
+    "revision": "c31ab9cfb31e515dfe41a04ec3bc829a",
+    "url": "/goit-react-hw-03-image-finder-infinite-scroll/static/media/icon-close.c31ab9cf.svg"
+  }
+]);
